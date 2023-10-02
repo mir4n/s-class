4ee81e42d7b9e01573ba5e69b00eebeaeadd0fb3
mir4n <miran.saman.work@gmail.com>
2023-10-02 07:22:25 +0300

    fixed bug

--- src/event-emitter.js
+++ src/event-emitter.js
@@ -26,11 +26,13 @@ function CEE(C) {
       (this._events[eventType] || []).forEach((listener) => listener(...args));
     }
     removeEventListener(eventType, target) {
-      const start = (this._events[eventType] || []).indexOf(target);
-      (this._events[eventType] || []).splice(start, 1);
+      if (this._events[eventType] instanceof Array) {
+        const start = this._events[eventType].indexOf(target);
+        this._events[eventType].splice(start, 1);
 
-      if (!Boolean(this._events[eventType].length)) {
-        delete this._events[eventType];
+        if (!Boolean(this._events[eventType].length)) {
+          delete this._events[eventType];
+        }
       }
     }
   };
